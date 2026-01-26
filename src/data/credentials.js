424807567715c4be918a02288ddc8393eb4a1ac9
William-Daniel-Maistry <166234162+William-Daniel-Maistry@users.noreply.github.com>
2026-01-26 17:40:12 +0200

    Add files via upload

--- src/data/credentials.js
+++ src/data/credentials.js
@@ -21,11 +21,16 @@ export const credentials = [
   },
   {
     id: 5,
+    title: "Certificate of Completion for Apptrepreneur - Systems Development",
+    link: "Apptrepreneur-Certificate-of-Completion.pdf"
+  },
+  {
+    id: 6,
     title: "Letter of Appreciation and Recommendation - FNB App Academy 2025 Support Team Manager",
     link: "Letter-of-Appreciation-and-Recommendation__FNB-App-Academy-2025-Support-Team-Manager.pdf"
   },
   {
-    id: 6,
+    id: 7,
     title: "Letter of Recommendation - Giving With A Purpose Foundation",
     link: "Letter-of-Recommendation__Giving-With-A-Purpose-Foundation.pdf"
   }
