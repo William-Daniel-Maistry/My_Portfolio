adebbb267044c6730fef16a4c35f0b5fc43aa86e
William-Daniel-Maistry <166234162+William-Daniel-Maistry@users.noreply.github.com>
2026-04-21 17:27:43 +0200

    Add files via upload

--- src/data/credentials.js
+++ src/data/credentials.js
@@ -6,31 +6,36 @@ export const credentials = [
   },
   {
     id: 2,
-    title: "Financial Management Academic Report",
-    link: "Skills-Academy__Academic-Report.pdf"
+    title: "Financial Management - Certificate of Achievement",
+    link: "Financial-Management__Certificate-of-Achievement.pdf"
   },
   {
     id: 3,
+    title: "Financial Management - Statement of Results",
+    link: "Financial-Management__Statement-of-Results.pdf"
+  },
+  {
+    id: 4,
     title: "Certificate of Completion for Introduction to App Development",
     link: "ITvarsity-Certificate_of_Completion.pdf"
   },
   {
-    id: 4,
+    id: 5,
     title: "Certificate in Full Stack Development - FNB App Academy 2025",
     link: "Certificate-in-Full-Stack-Development__FNB-App-Academy-2025.pdf"
   },
   {
-    id: 5,
+    id: 6,
     title: "Certificate of Completion for Apptrepreneur - Systems Development",
     link: "Apptrepreneur-Certificate-of-Completion.pdf"
   },
   {
-    id: 6,
+    id: 7,
     title: "Letter of Appreciation and Recommendation - FNB App Academy 2025 Support Team Manager",
     link: "Letter-of-Appreciation-and-Recommendation__FNB-App-Academy-2025-Support-Team-Manager.pdf"
   },
   {
-    id: 7,
+    id: 8,
     title: "Letter of Recommendation - Giving With A Purpose Foundation",
     link: "Letter-of-Recommendation__Giving-With-A-Purpose-Foundation.pdf"
   }
